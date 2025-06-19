b403fee34acfedf318044d8987c459484c9db370
edo37 <edoardo0307@gmail.com>
2025-06-19 12:00:28 +0200

    modified comment about scroll-padding-top

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// setting scroll-padding-top property value with the exact the navbar height
+// setting scroll-padding-top css property value with the exact navbar height
 const navigationHeight = document.querySelector('nav').offsetHeight
 document.documentElement.style.setProperty('--navHeight', navigationHeight + 'px');
 
